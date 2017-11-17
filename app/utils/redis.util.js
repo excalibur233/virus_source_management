9e97f6c9f0409f81c1fa7ab6240d4fc3171fbfa6
wangyg <wyg569983202@qq.com>
2017-11-17 16:30:41 +0800

    更新redis

--- app/utils/redis.util.js
+++ app/utils/redis.util.js
@@ -1,5 +1,5 @@
 const constants = require('../../config/constants');
-// 获取redisKey  和virus_source中保持一致
+// 获取redisKey
 exports.getRedisPrefix = (type, id = '') => {
   let redisKey = '';
   switch (type) {
@@ -13,27 +13,33 @@ exports.getRedisPrefix = (type, id = '') => {
       redisKey = `${constants.REDIS_PREFIX}|rank|comment|news`; // 文章评论总排行 rank|comment|news {newsId:num}
       break;
     case 3:
-      redisKey = `${constants.REDIS_PREFIX}|rank|user|pv_news`; // 用户分享出的文章 浏览pv总排行 rank|user|pv_news:userId {newsId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|rank|news|user_share`; // 用户分享出的文章 新闻pv日排行 rank|news|user_share:uid:20171116:type {newsId:num}
       break;
     case 4:
-      redisKey = `${constants.REDIS_PREFIX}|rank|user|uv_news`; // 用户分享出的文章 浏览uv总排行 rank|user|uv_news:userId {newsId:uv_num}
+      redisKey = `${constants.REDIS_PREFIX}|rank|news_type|user_share`; // 用户分享出的文章 新闻类别pv日排行  rank|news_type|user_share:uid:20171117 {type:pv_num}
       break;
     case 7:
-      redisKey = `${constants.REDIS_PREFIX}|rank|user|pv_products`; // 用户分享出的商品 浏览pv总排行  rank|user|pv_products:userId {productId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|rank|pdt_buy|user_share`; // 用户分享的商品，商品购买日排行 rank|pdt_buy|user_share:uid:20171116:type {productId:num}
       break;
     case 8:
-      redisKey = `${constants.REDIS_PREFIX}|rank|user|uv_products`; // 用户分享出的商品 浏览uv总排行 rank|user|uv_products:userId {productId:uv_num}
+      redisKey = `${constants.REDIS_PREFIX}|rank|pdt_buy_type|user_share`; // 用户分享的商品，商品类别购买日排行 rank|pdt_buy_type|user_share:uid_20171116 {type:num}
+      break;
+    case 23:
+      redisKey = `${constants.REDIS_PREFIX}|rank|pdt|user_share`; // 用户分享的商品，商品pv日排行 rank|pdt|user_share:uid:20171116:type {productId:num}
+      break;
+    case 24:
+      redisKey = `${constants.REDIS_PREFIX}|rank|pdt_type|user_share`; // 用户分享的商品，商品pv类别日排行 rank|pdt_type|user_share:uid:20171116 {type:num}
       break;
 
 
     case 5:
-      redisKey = `${constants.REDIS_PREFIX}|data|user|pv_news`; // 用户所有分享文章的日 uv、pv记录  data|user|pv_news:userId:20171020 {viewerId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|data|user|pv_news`; // 用户当日分享所有文章  浏览人记录  data|user|pv_news:uid:20171020 {viewerId:pv_num}
       break;
     case 9:
-      redisKey = `${constants.REDIS_PREFIX}|data|user|pv_products`; // 用户分享所有商品日 uv、pv记录 data|user|pv_products:20171020 {viewerId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|data|user|pv_products`; // 用户当日分享所有商品 浏览人记录 data|user|pv_products:uid:20171020 {viewerId:pv_num}
       break;
     case 10:
-      redisKey = `${constants.REDIS_PREFIX}|data|user|purchase_record`; // 用户分享商品后，日购买记录
+      redisKey = `${constants.REDIS_PREFIX}|data|user|purchase_record`; // 用户当日分享所有商品，购买人买记录 data|user|purchase_record:uid:20171116 {userId:buy_times}
       break;
 
 
@@ -51,6 +57,9 @@ exports.getRedisPrefix = (type, id = '') => {
     case 12:
       redisKey = `${constants.REDIS_PREFIX}|brief_rank_info|products`; // 一些排行中需要显示的精简product信息
       break;
+    case 25:
+      redisKey = `${constants.REDIS_PREFIX}|brief_rank_info|users`; // 一些排行中需要显示的精简user信息
+      break;
 
 
     case 14:
@@ -62,7 +71,7 @@ exports.getRedisPrefix = (type, id = '') => {
       redisKey = `${constants.REDIS_PREFIX}|log|pv|news`; // 某文章浏览人次、人数记录 log|pv|news:newsId {userId:pv_num}
       break;
     case 16:
-      redisKey = `${constants.REDIS_PREFIX}|log|pv|user|news`; // 某用户分享出的某文章浏览人次、人数记录 log|pv|user|news:newsId:uid_1:20171020 {viewerId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|log|pv|user|news`; // 某用户分享出的某文章浏览人次、人数记录 log|pv|user|news:newsId:uid:20171020 {viewerId:pv_num}
       break;
     case 17:
       redisKey = `${constants.REDIS_PREFIX}|log|thumbUp|news`; // 某文章点赞人次、人数记录
@@ -71,13 +80,13 @@ exports.getRedisPrefix = (type, id = '') => {
       redisKey = `${constants.REDIS_PREFIX}|log|pv|products`; // 某商品浏览人次、人数记录 log|pv|products:productId {userId:pv_num}
       break;
     case 20:
-      redisKey = `${constants.REDIS_PREFIX}|log|pv|user|products`; // 某用户分享出的某商品浏览人次、人数记录 log|pv|user|products:pid:uid_1:20171020 {viewerId:pv_num}
+      redisKey = `${constants.REDIS_PREFIX}|log|pv|user|products`; // 某用户分享出的某商品浏览人次、人数记录 log|pv|user|products:pid:uid:20171020 {viewerId:pv_num}
       break;
     case 21:
       redisKey = `${constants.REDIS_PREFIX}|log|transmit|news`; // 某文章转发人次、人数记录 log|transmit|news:newsId {userId:num}
       break;
     case 22:
-      redisKey = `${constants.REDIS_PREFIX}|log|transmit|user|news`; // 某用户分享出的某文章转发人次、人数 log|transmit|user|news:newsId:uid_1:20171020 {uid:_num}
+      redisKey = `${constants.REDIS_PREFIX}|log|transmit|user|news`; // 某用户分享出的某文章转发人次、人数 log|transmit|user|news:newsId:uid:20171020 {uid:_num}
       break;
 
 
