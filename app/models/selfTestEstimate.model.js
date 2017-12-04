8e9f505dc54be1c35507c8778756317b7ed7b8b2
wangyg <wyg569983202@qq.com>
2017-12-04 16:19:06 +0800

    自测题 修改了一个数据表结构

--- app/models/selfTestEstimate.model.js
+++ app/models/selfTestEstimate.model.js
@@ -7,11 +7,15 @@ module.exports = function (sequelize, DataTypes) {
     // 对应的资讯id
     newsId: { type: DataTypes.BIGINT, allowNull: false },
     // 分数段 最小值
-    minScore: { type: DataTypes.INTEGER, allowNull: false},
+    minScore: { type: DataTypes.INTEGER, allowNull: false },
     // 分数段 最大值
     maxScore: { type: DataTypes.INTEGER },
-    // 评价
+    // 评价结果 为了防止不同自测题给出的结果展示图不一样  这里采用富文本的方式
     estimate: { type: DataTypes.TEXT },
+    // 分享文案
+    shareContext: { type: DataTypes.TEXT },
+    // 相关推荐
+    recommendContext: { type: DataTypes.TEXT },
   }, {
     timestamps: false,
     paranoid: true,
