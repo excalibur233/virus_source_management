ff8753cc119b8a0c23691ce9678b60eff44d5d84
junfeng0202 <1628439099@qq.com>
2018-02-05 10:50:14 +0800

    '数据库结构修改后的代码调整'

--- config/constants.js
+++ config/constants.js
@@ -1,13 +1,31 @@
 const constants = {
-  // redis_data前缀
-  REDIS_PREFIX: 'shareTest',
+	// redis_data前缀
+	REDIS_PREFIX: 'med',
 
-  // api const
-  HTTP_FAIL: 500,
-  HTTP_SUCCESS: 200,
+	// api const
+	HTTP_FAIL: 500,
+	HTTP_SUCCESS: 200,
 
-  // 新闻列表的分页显示数量
-  NEWS_PAGE_LIMIT: 25,
+	// 新闻列表的分页显示数量
+	NEWS_PAGE_LIMIT: 25,
+
+	// 资讯搜索类型 热门
+	HOT_NEWS: 1,
+	// 资讯搜索类型 最新
+	LATEST_NEWS: 2,
+
+	// 资讯内容类型 默认为无类别
+	CONTEXT_TOTAL: 0,
+
+	// 资讯类型 普通新闻
+	TYPE_NEWS: 1,
+	// 资讯类型 自测题
+	TYPE_ESTIMATE: 2,
+
+	// 资讯分类id对应列表   目前写死
+	NEWS_CLASS_LIST: {
+		0: '全部', 1: '痛风', 2: '糖尿病', 3: '甲状腺', 4: '自测题',
+	},
 
 };
 
--- config/redis.js
+++ config/redis.js
@@ -1,25 +1,42 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
 const config = require('./config');
-const logger = require('../app/utils/log.util').getLogger('errLogger');
 
-// 目前: 管理后台至操作4库，其他的库不要使用了
-// 客户端session记录在0库
-// 客户端业务数据记录(除了积分和佣金总额)在4库  这里的用处主要是在增加资讯的时候加入热门排行榜
-// 客户端微信信息的token存在2库
-// 客户端积分和佣金总额在3库
-module.exports = function () {
-  const redisConfig = config.redisConfig || { host: '127.0.0.1', port: '6379' };
+// note: nodejs和php不同之处：
+// 如果是php，这里一定要是全局变量才能实现单例模式，
+// 而nodejs exports以后每次读的都是上次的缓存
+const globalRedisClient = {};
+// 目前:
+// redis-session存在0库  这个主要存储session
+// 业务数据记录在4库
+// 获取微信信息的token存在2库
 
-  const client = redis.createClient(redisConfig);
+// 所有项目的总积分存在3库里 便于通用积分商城的使用
+module.exports = function (type = 0) {
+	const redisConfig = config.redisConfig || { host: '127.0.0.1', port: '6379' };
+	const clientType = type ? parseInt(type, 0) : 0;
 
-  bluebird.promisifyAll(redis.RedisClient.prototype);
+	if (!globalRedisClient[clientType]) {
+		if (clientType === 1) {
+			redisConfig.db = 4;
+		} else if (clientType === 2) {
+			redisConfig.db = 2;
+		} else if (clientType === 3) {
+			redisConfig.db = 3;
+		} else {
+			redisConfig.db = 0;
+		}
 
-  bluebird.promisifyAll(redis.Multi.prototype);
+		const client = redis.createClient(redisConfig);
+		bluebird.promisifyAll(redis.RedisClient.prototype);
 
-  client.on('error', (err) => {
-    logger.error(`Error: ${err}`);
-  });
+		bluebird.promisifyAll(redis.Multi.prototype);
 
-  return client;
+		client.on('error', (err) => {
+			console.log(`Error: ${err}`);
+		});
+		globalRedisClient[clientType] = client;
+	}
+
+	return globalRedisClient[clientType];
 };
